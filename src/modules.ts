65f42a8e13ee62039fb49e803e143803acaab536
thaint2908 <thai.nt05112000@gmail.com>
2022-11-06 22:49:29 +0700

    fix: conflic master

--- src/modules.ts
+++ src/modules.ts
@@ -3,6 +3,13 @@ import { AdminModule } from './modules/admin/admin.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { CategoryModule } from './modules/category/category.module';
 import ormConfig from './ormconfig';
+import { FilesModule } from './modules/files/files.module';
 
-const Modules = [AuthModule, AdminModule, CategoryModule, TypeOrmModule.forRootAsync({ useFactory: ormConfig })];
+const Modules = [
+    AuthModule,
+    FilesModule,
+    AdminModule,
+    CategoryModule,
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+];
 export default Modules;
